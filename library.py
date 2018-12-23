214b1d7bc85c88096d20a7146e55229c10ac4570
varinyam <varinyam@gmail.com>
2018-12-23 13:12:53 +0530

    adding library py

---
+++ library.py
@@ -0,0 +1,26 @@
+from abc import ABCMeta,abstractmethod
+import abc
+class Shape(object):
+    __metaclass__ = ABCMeta
+    @abc.abstractmethod
+    def area(self):
+        return 0
+
+class Square(Shape):
+    width = 5
+    length =10
+    def area(self):
+        print self.width * self.length
+
+class Rectangle(Shape):
+    side = 4
+    def area(self):
+        print self.side * 2
+
+sq = Square()
+rect = Rectangle()
+rect.area()
+sq.area()
+
+
+
